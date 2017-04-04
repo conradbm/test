9ef2ddd3a393fc2c3dec19a84c4a424ba08ea7db
Blake Conrad <bmconrad@iupui.edu>
2017-04-04 12:26:22 -0400

    stored lat/long/type into dataframe object

--- data_pulled.R
+++ data_pulled.R
@@ -3,9 +3,25 @@
 library("rjson")
 json_file <- "https://data.cityofchicago.org/resource/6zsd-86xi.json"
 json_data <- fromJSON(paste(readLines(json_file), collapse=""))
-lat1 <- json_data[[1]]$latitude
-lon1 <- json_data[[1]]$longitude
 
-df <- data.frame(matrix(unlist(json_data), nrow=1000, byrow=T),stringsAsFactors=FALSE)
+fillDataFrameWithList <- function(someList){
 
-head(df)
+  df <- data.frame(latitude=c(0), longitude=c(0), primary_type=c(""))
+  df$latitude <- as.numeric(df$latitude)
+  df$longitude <- as.numeric(df$longitude)
+  df$primary_type <- as.character(df$primary_type)
+  
+  for(i in 1:length(someList)){
+      #df[i,1] <- 
+      #df[i,2] <-
+      #df[i,3] <- 
+     cat(someList[[i]]$latitude, " ", someList[[i]]$longitude," ", someList[[i]]$primary_type , "\n")
+      row <- c(someList[[i]]$latitude, someList[[i]]$longitude, someList[[i]]$primary_type)
+      df <- rbind(df, row)
+  }
+  
+  return(df)
+}
+
+newdf <- fillDataFrameWithList(json_data)
+head(newdf)
