98521a93b83c569535482c957cdd1a27a213176f
jnemery <jennifernicoleemery@gmail.com>
2017-04-04 10:19:21 -0400

    add map

--- server.R
+++ server.R
@@ -8,6 +8,7 @@
 # https://data.cityofchicago.org/resource/6zsd-86xi.json
 
 library(shiny)
+library(ggmap)
 
 # Define server logic required to draw a histogram
 shinyServer(function(input, output) {
@@ -19,7 +20,10 @@ shinyServer(function(input, output) {
     bins <- seq(min(x), max(x), length.out = input$bins + 1)
     
     # draw the histogram with the specified number of bins
-    hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    # hist(x, breaks = bins, col = 'darkgray', border = 'white')
+    map <- get_map(location = "chicago", zoom = 11)
+    ggmap(map) 
+    #+ geom_point(data=crime_data[crime_data$Primary.Type=="HOMICIDE",], aes(x=Longitude, y=Latitude))
     
   })
   
